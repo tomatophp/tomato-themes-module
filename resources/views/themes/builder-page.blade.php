6fcb352f2d43b7292f0acd34ef11079f0586e4d6
Fady Mondy <info@3x1.io>
2024-03-20 16:24:48 +0200

    update translations input

--- resources/views/themes/builder-page.blade.php
+++ resources/views/themes/builder-page.blade.php
@@ -9,7 +9,7 @@
             'en' => $model->getTranslation('body', 'en') ?? '',
         ]
     ]">
-        <x-tomato-translation :label="__('Body')" name="body" type="rich" />
+        <x-tomato-translation :label="__('Body')" name="body" type="markdown" />
         <div class="flex justify-start gap-2 pt-3">
             <x-tomato-admin-submit  label="{{__('Save')}}" :spinner="true" />
             <x-tomato-admin-button secondary type="button" @click.prevent="modal.close" label="{{__('Cancel')}}"/>
